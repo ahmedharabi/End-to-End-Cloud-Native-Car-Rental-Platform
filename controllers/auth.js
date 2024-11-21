36b48b744c035ebce7850979b7a10222d9e5523d
Ahmed Harabi <harabiahmed88@gmail.com>
2024-11-21 18:16:06 +0100

    adding login and sign up

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
 const User=require("../models/user");
 const bcrypt=require("bcryptjs");
 const {where} = require("sequelize");
+const jwt=require("jsonwebtoken");
+require("dotenv").config({path: "../.env"});
 
 
 exports.register= async (req,res)=>{
@@ -20,9 +22,38 @@ exports.register= async (req,res)=>{
             password:hashedPassword
         })
         return res.status(201).json({message:"user created succefully"})
-        }catch (err){
-            return res.status(500).json({message: err})
+    }catch (err){
+        return res.status(500).json({message: err})
+    }
+
+
+}
+exports.login=async (req,res)=>{
+    try{
+        const{email,password}=req.body;
+        if(!email || !password){
+            return res.status(422).json({message:"please fill in all the fields"});
         }
+        const user= await User.findOne({where:{email:email}});
 
+        if(!user){
+            return res.status(401).json({message:"email or password is invalid"});
+        }
+        const passwordMatch=await bcrypt.compare(password,user.password);
 
+        if(!passwordMatch){
+            return res.status(401).json({message:"email or password is invalid"});
+        }
+        const accessToken=jwt.sign({userId:user.id},process.env.SECRET_KEY,{subject:"accessApi",expiresIn: "1h"});
+        return res.status(200).json({
+            id:user.id,
+            email:user.email,
+            name:user.name,
+            accessToken:accessToken
+        })
+
+    }
+    catch (err){
+        return res.status(500).json({message: err})
+    }
 }
--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,6 @@ const router=express.Router();
 const authController=require("../controllers/auth");
 
 router.post("/register",authController.register);
-//router.post("/login",login);
+router.post("/login",authController.login);
 
 module.exports=router;
