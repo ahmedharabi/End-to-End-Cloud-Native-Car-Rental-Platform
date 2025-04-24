0f63fa72c1630fea98b741b67b85bf7944874c08
Ahmed Harabi <harabiahmed88@gmail.com>
2025-04-24 11:08:11 +0100

    added docker compose

--- util/database.js
+++ util/database.js
@@ -1,10 +1,10 @@
 const Sequelize=require("sequelize");
 require("dotenv").config();
-const sequelize=new Sequelize(
-    "carrent",
-    "root",
-    "ahmedahmed",
+const sequelize = new Sequelize('carrent', 'root', 'ahmedahmed', {
+    host: 'localhost',      // or the Docker service name if inside another container (e.g. 'mysql')
+    dialect: 'mysql',
+    port: 3306,              // default MySQL port
+    logging: false           // optional: disable SQL logging in console
+});
 
-    {dialect:"mysql",host:"localhost"}
-)
 module.exports=sequelize;
